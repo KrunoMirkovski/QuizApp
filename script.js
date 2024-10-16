c5853e05b60586f9ebebaf25dcfc2e34b998d86e
KRUNISLAV MIRKOVSKI <kmirkovski@gmial.com>
2024-10-16 15:03:24 +0200

    creating Javascript for exit button

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const startBtn = document.querySelector(".btn_start");
 const popUp = document.querySelector(".popup");
+const exitBtn = document.querySelector(".btn_exit");
+const main = document.querySelector(".main");
 
 startBtn.onclick = () => {
   popUp.classList.add("active");
+  main.classList.add("active");
+};
+
+exitBtn.onclick = () => {
+  popUp.classList.remove("active");
+  main.classList.remove("active");
 };
