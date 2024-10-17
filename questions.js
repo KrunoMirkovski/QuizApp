af57695808ba56be7956be3b9e84c2de13dc1454
KRUNISLAV MIRKOVSKI <kmirkovski@gmial.com>
2024-10-17 12:15:55 +0200

    adding questions for the quiz

---
+++ questions.js
@@ -0,0 +1,72 @@
+JavaScript Quiz Questions
+
+    What does typeof operator do in JavaScript?
+        A) Checks the type of a variable
+        B) Converts data to a string
+        C) Declares a new variable
+        D) Checks if a variable is undefined
+        Answer: A
+
+    Which company developed JavaScript?
+        A) Google
+        B) Netscape
+        C) Microsoft
+        D) Mozilla
+        Answer: B
+
+    How do you write "Hello World" to the console in JavaScript?
+        A) console.write("Hello World")
+        B) print("Hello World")
+        C) console.log("Hello World")
+        D) document.log("Hello World")
+        Answer: C
+
+    What will 2 + '2' evaluate to in JavaScript?
+        A) 4
+        B) "22"
+        C) NaN
+        D) Error
+        Answer: B
+
+    What keyword is used to declare a constant variable in JavaScript?
+        A) let
+        B) var
+        C) const
+        D) constant
+        Answer: C
+
+    Which of the following is not a JavaScript data type?
+        A) Boolean
+        B) Number
+        C) Undefined
+        D) Character
+        Answer: D
+
+    What is the output of Boolean('')?
+        A) true
+        B) false
+        C) null
+        D) undefined
+        Answer: B
+
+    Which loop is guaranteed to run at least once?
+        A) for loop
+        B) while loop
+        C) do-while loop
+        D) None of the above
+        Answer: C
+
+    What is the purpose of the isNaN() function?
+        A) To check if a value is a number
+        B) To check if a value is NaN (Not-a-Number)
+        C) To convert a value to a number
+        D) To check if a value is an integer
+        Answer: B
+
+    Which method can be used to convert an array to a string in JavaScript?
+
+        A) join()
+        B) concat()
+        C) split()
+        D) toString()
+        Answer: A
