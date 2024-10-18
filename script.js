2af525020612c513f026ffb44708d6c7fb262e7e
KRUNISLAV MIRKOVSKI <kmirkovski@gmial.com>
2024-10-18 14:33:19 +0200

    Configurating the Continue button

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const startBtn = document.querySelector(".btn_start");
 const popUp = document.querySelector(".popup");
 const exitBtn = document.querySelector(".btn_exit");
 const main = document.querySelector(".main");
+const continueBtn = document.querySelector(".btn_continue");
+const quiz = document.querySelector(".quiz");
 
 startBtn.onclick = () => {
   popUp.classList.add("active");
@@ -9,6 +11,12 @@ startBtn.onclick = () => {
 };
 
 exitBtn.onclick = () => {
+  popUp.classList.remove("active");
+  main.classList.remove("active");
+};
+
+continueBtn.onclick = () => {
+  quiz.classList.add("active");
   popUp.classList.remove("active");
   main.classList.remove("active");
 };
